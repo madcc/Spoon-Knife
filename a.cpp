ccd9d55b757b56868a0f3641148316be6f0d0555
madcc <madcc09@163.com>
2013-10-19 12:07:51 +0800

    change a.cpp

--- a.cpp
+++ a.cpp
@@ -17,3 +17,16 @@
  */
 #include <stdlib.h>
 
+#include	<stdlib.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
